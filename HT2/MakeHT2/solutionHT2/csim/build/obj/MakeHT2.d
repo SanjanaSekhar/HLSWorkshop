fa2d4289c59104fa589ec1ca505fd1922d09f9d3
Sanjana Sekhar <ssekhar@uwlogin.cern.ch>
2017-09-15 17:57:34 +0200

    log files

--- HT2/MakeHT2/solutionHT2/csim/build/obj/MakeHT2.d
+++
@@ -1,3 +0,0 @@
-obj/MakeHT2.o: ../../../../MakeHT2.c \
-  /data/tools/Vivado_HLS/2016.4/common/technology/autopilot/etc/autopilot_ssdm_op.h \
-  ../../../../MakeHT2.h
--- HT2/MakeHT2/solutionHT2/csim/build/obj/TestMakeHT2.d
+++
@@ -1,3 +0,0 @@
-obj/TestMakeHT2.o: ../../../../TestMakeHT2.c \
-  /data/tools/Vivado_HLS/2016.4/common/technology/autopilot/etc/autopilot_ssdm_op.h \
-  ../../../../MakeHT2.h
