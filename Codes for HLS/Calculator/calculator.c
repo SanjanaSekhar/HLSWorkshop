22586f9d916863bdf1a8f30f129b9150c4763cca
Sanjana Sekhar <ssekhar@uwlogin.cern.ch>
2017-09-22 11:27:15 +0200

    updates to calculator.c

--- Codes for HLS/Calculator/calculator.c
+++ Codes for HLS/Calculator/calculator.c
@@ -1,11 +1,11 @@
 /* try the following: 
--no default/default
--no directives/directives
---UNROLL
---ARRAY_PARTITION
---PIPELINE
---DEPENDENCE
---DATAFLOW
+-no default/default-- no diff
+-no directives/directives-- clock needed to be adjusted
+--UNROLL-- look in comments
+--ARRAY_PARTITION-- 33 to 21 on 9.09 clock, 70 to 58 in 4.16 clock, w/o increases estimated clock, with decreases
+--PIPELINE-- max latency w/o this is 501, with this is 33, II changes nothing
+--DEPENDENCE-- no diff
+--DATAFLOW-- reverses effects of pipeline
 -no LUT/LUT for mul
 -no LUT/LUT for div
 */
@@ -18,12 +18,21 @@ void calculate(double A[20],double B[20],double C[20],int select)
 {
 	//calculate the sum/difference/product/quotient of 2 arrays based on select.
 	int i;
+	#pragma HLS PIPELINE
+	//Note: unable to load B array for select=6 warning goes away after ARRAY_PARTITION
+	#pragma HLS ARRAY_PARTITION variable=A complete
+	#pragma HLS ARRAY_PARTITION variable=B complete
+	#pragma HLS ARRAY_PARTITION variable=C complete
+	//#pragma HLS DEPENDENCE
+	//#pragma HLS DATAFLOW
+	//#pragma HLS UNROLL-- made no diff, applied automatically?
 	switch(select)
 	{
 		case 1:
 		{
 			for(i=0;i<20;i++)
 			{
+				//#pragma HLS UNROLL-- made no diff
 				C[i]=A[i]+B[i];
 			}
 			break;
@@ -68,6 +77,8 @@ void calculate(double A[20],double B[20],double C[20],int select)
 			}
 			break;
 		}
+		//default: printf("nope");
+
 	}
 }
 
